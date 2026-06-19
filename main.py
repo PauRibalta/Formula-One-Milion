bd2fb8357c2cbc417cdb0cd0f04d3d8c64a57618
Pau Ribalta <pauribaltagabarro@gmail.com>
2026-06-19 18:28:38 +0200

    V2- Add driver and car race simulation

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from src.models.driver import Driver
+from src.models.car import Car
 from src.simulation.race import simulate_race
 
-drivers = [
-    Driver("Verstappen", 98, 99, 100, 95, 97, 96, 94, 93, 92, 91, 95, 98, 97, 96, 98),
-    Driver("Norris", 95, 96, 95, 90, 94, 92, 90, 89, 88, 90, 92, 93, 92, 91, 93),
-    Driver("Leclerc", 94, 95, 94, 92, 93, 91, 89, 90, 91, 90, 91, 92, 91, 92, 91),
+
+Verstappen = Driver("Verstappen", 98, 99, 100, 95, 97, 96, 94, 93, 92, 91, 95, 98, 97, 96, 98)
+Norris = Driver("Norris", 95, 96, 95, 90, 94, 92, 90, 89, 88, 90, 92, 93, 92, 91, 93)
+Leclerc = Driver("Leclerc", 94, 95, 94, 92, 93, 91, 89, 90, 91, 90, 91, 92, 91, 92, 91)
+
+
+
+RB26 = Car("RB26", 96, 97, 99, 98, 100, 95, 95, 95, 92, 100, 99, 92, 94, 95, 94, 96, 93, 95)
+MCL40 = Car("MCL40", 97, 96, 96, 99, 98, 94, 97, 94, 94, 98, 97, 95, 96, 94, 95, 94, 96, 94)
+SF26 = Car("SF-26", 100, 98, 95, 96, 95, 90, 92, 99, 91, 94, 94, 91, 93, 93, 94, 95, 92, 93)
+
+
+entries = [
+    (Verstappen, RB26),
+    (Norris, MCL40),
+    (Leclerc, SF26)
 ]
 
-result = simulate_race(drivers)
+results = simulate_race(entries)
 
-for i, d in enumerate(result):
-    print(f"{i+1}. {d.name}")
+print("\n=== RACE RESULTS ===\n")
+
+for position, (driver, score) in enumerate(results, start=1):
+    print(f"{position}. {driver.name} - {score:.2f}")
--- src/simulation/race.py
+++ src/simulation/race.py
@@ -1,12 +1,44 @@
 import random
 
-def simulate_race(drivers):
+
+def calculate_car_performance(car):
+    return (
+        car.top_speed +
+        car.acceleration +
+        car.low_speed_corners +
+        car.medium_speed_corners +
+        car.high_speed_corners +
+        car.dirty_air_tolerance +
+        car.tyre_management +
+        car.engine_power +
+        car.engine_cooling +
+        car.aerodynamics +
+        car.downforce +
+        car.fuel_efficiency +
+        car.reliability +
+        car.energy_recovery +
+        car.battery_capacity +
+        car.energy_deployment +
+        car.hybrid_reliability +
+        car.energy_strategy
+    ) / 18
+
+
+def simulate_race(entries):
     results = []
 
-    for driver in drivers:
-        score = driver.race_pace + random.randint(-5, 5)
+    for driver, car in entries:
+
+        car_performance = calculate_car_performance(car)
+
+        score = (
+            driver.race_pace * 0.6 +
+            car_performance * 0.4 +
+            random.uniform(-5, 5)
+        )
+
         results.append((driver, score))
 
-    results.sort(key=lambda x: x[1], reverse=True)
+    results.sort(key=lambda result: result[1], reverse=True)
 
-    return [d[0] for d in results]
+    return results
