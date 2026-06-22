cd0df06334ea45d19d18f7d0942b0d8731d608c7
Pau Ribalta <pauribaltagabarro@gmail.com>
2026-06-22 18:53:35 +0200

    Add team model and complete 2026 team data

---
+++ src/data/teams_data.py
@@ -0,0 +1,23 @@
+from src.models.team import Team
+
+mercedes = Team("Mercedes",98, 98, 98,99, 98,98, 98, 98, 98, 97)
+
+mclaren = Team("McLaren",98, 97, 97,97, 97,97,96, 97,97,97)
+
+red_bull = Team("Red Bull",99, 100, 98,95, 96,96,95, 95,93,98)
+
+ferrari = Team("Ferrari",93, 93, 94,96, 96,95,97, 98,97,93)
+
+aston_martin = Team("Aston Martin",90, 90, 91,94, 96,90,93, 93,92,86)
+
+alpine = Team("Alpine",89, 89, 89,89, 89,89,89, 89,89,89)
+
+racing_bulls = Team("Racing Bulls",89, 90, 88,88, 87,87,87, 87,87,86)
+
+audi = Team("Audi",86, 86, 86,88, 87,86,87, 87,88,85)
+
+williams = Team("Williams",86, 86, 86,86, 86,85,87, 86,86,86)
+
+haas = Team("Haas",84, 85, 84,84, 83,84,84, 83,85,84)
+
+cadillac = Team("Cadillac",81, 82, 81,84, 83,81,82, 82,84, 80)
